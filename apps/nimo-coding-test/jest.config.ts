804c39ee32c57f1dd84acf2ff4cab8d03a36dd6d
Dhian <muthidp@gmail.com>
2024-11-18 16:56:47 +1100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/QkaMyvLD2T

---
+++ apps/nimo-coding-test/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'nimo-coding-test',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nimo-coding-test',
+};
